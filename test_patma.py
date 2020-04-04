bc085f467c81e6efcfbea697e79b65e7990fb997
Guido van Rossum <guido@python.org>
2020-04-04 14:06:33 -0700

    patma.py
    Add more tests

--- test_patma.py
+++ test_patma.py
@@ -27,6 +27,31 @@ def test_alternatives_pattern():
     assert pat.match('1') is None
 
 
+def test_variable_pattern():
+    # case x:
+    pat = VariablePattern('x')
+    assert pat.match(42) == {'x': 42}
+    assert pat.match((1, 2)) == {'x': (1, 2)}
+    assert pat.match(None) == {'x': None}
+
+
+def test_annotated_pattern():
+    # case (x: int):
+    pat = AnnotatedPattern(VariablePattern('x'), int)
+    assert pat.match(42) == {'x': 42}
+    assert pat.match('hello') is None
+
+
+def test_sequence_pattern():
+    # case (x, y, z):
+    pat = SequencePattern([VariablePattern(s) for s in 'xyz'])
+    assert pat.match((1, 2, 3)) == {'x': 1, 'y': 2, 'z': 3}
+    assert pat.match((1, 2)) is None
+    assert pat.match((1, 2, 3, 4)) is None
+    assert pat.match(123) is None
+    assert pat.match('abc') is None
+
+
 def test_instance_pattern():
     # case MyClass(xx: int, y='hello'):
     vxx = AnnotatedPattern(VariablePattern('xx'), int)
