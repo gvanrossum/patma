ad0ee4d3f1248e9484d338575bfe6e6096f09d8a
Guido van Rossum <guido@python.org>
2020-06-11 09:43:59 -0700

    Use match/case in TokenStream.next()

--- examples/math.py
+++ examples/math.py
@@ -18,12 +18,14 @@ class TokenStream:
         while True:
             try:
                 token = next(self.stream)
-                if token.type == tokenize.NEWLINE:
-                    continue
-                token_type = token.type
-                if token.string in ("(", ")"):
-                    token_type = tokenize.EXACT_TOKEN_TYPES[token.string]
-                self.token = (token_type, token.string)
+                match [token.type, token.string]:
+                    case [tokenize.NEWLINE, _]:
+                        continue
+                    case [_, token_string := "("|")"]:
+                        token_type = tokenize.EXACT_TOKEN_TYPES[token_string]
+                    case [token_type, token_string]:
+                        pass
+                self.token = (token_type, token_string)
                 self.pos = token.start[1]
                 break
             except StopIteration:
